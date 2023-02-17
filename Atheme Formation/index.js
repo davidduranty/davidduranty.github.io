d86f218606a028650ac7affde7344412ec7de801
DavidDuranty <dadaduranty@gmail.com>
2023-02-17 09:07:26 +0100

    Maj decouverte v.8 li sous veille switch

--- Atheme Formation/index.js
+++ Atheme Formation/index.js
@@ -21,13 +21,58 @@ const search = document.querySelector(".search");
 const veille = document.querySelector(".veille-nav");
 const theme = document.querySelector(".theme");
 
-console.log(veille);
 icon.addEventListener("click", (e) => {
   e.preventDefault();
   search.classList.toggle("show-response");
 });
+
 veille.addEventListener("mouseover", () => {
   theme.classList.toggle("theme-visible");
+});
+// theme.addEventListener('mouseout', () => {
+//   theme.style.opacity = 0;
+// })
+theme.addEventListener("click", (e) => {
+  switch (e.target.id) {
+    case "numerique":
+      window.location.href = "http:numerique.html";
+      break;
+    case "envir":
+      window.location.href = "http:environnement.html";
+      break;
+    case "sante":
+      window.location.href = "http:sante.html";
+      break;
+    case "mobilite":
+      window.location.href = "mobilite.html";
+      break;
+    case "btp":
+      window.location.href = "btp.html";
+      break;
+    case "commerce":
+      window.location.href = "commerce.html";
+      break;
+    case "culture":
+      window.location.href = "culture.html";
+      break;
+    case "cohesion":
+      window.location.href = "cohesion.html";
+      break;
+    case "industrie":
+      window.location.href = "industrie.html";
+      break;
+    case "finance":
+      window.location.href = "finance.html";
+      break;
+    case "service":
+      window.location.href = "";
+      break;
+    case "competence":
+      window.location.href = "";
+      break;
+    default:
+      null;
+  }
 });
 
 menu.forEach((clicked) => {
