d4afd5adbc24de6e6531e61b0c6889cb9bda0722
DavidDuranty <dadaduranty@gmail.com>
2023-04-24 17:26:12 +0200

    Exercice Site resto Css + Animate V.5

--- Site resto/script.js
+++ Site resto/script.js
@@ -1,7 +1,24 @@
 let typed = new Typed(".textDefil", {
-    strings: ["CALIFORNIA"],
-    typeSpeed: 300,
-    backSpeed: 100,
-    backDelay: 1000,
-    loop: true,
-})
+  strings: ["CALIFORNIA"],
+  typeSpeed: 300,
+  backSpeed: 100,
+  backDelay: 1000,
+  loop: true,
+});
+const slide = document.querySelector(".photo-swiper");
+const right = document.getElementById("layer+");
+const left = document.getElementById("layer-");
+
+function changeNext() {
+  right.addEventListener("click", () => {
+    slide.scrollLeft += 360;
+  });
+}
+
+function changePrevious() {
+  left.addEventListener("click", () => {
+    slide.scrollLeft -= 360;
+  });
+}
+changeNext();
+changePrevious()
