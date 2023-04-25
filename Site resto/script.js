cf1962b2955c180107659dda1a573ed8439d3f8f
DavidDuranty <dadaduranty@gmail.com>
2023-04-25 15:39:41 +0200

    Exercice Site resto intégration grid

--- Site resto/script.js
+++ Site resto/script.js
@@ -6,19 +6,32 @@ let typed = new Typed(".textDefil", {
   loop: true,
 });
 const slide = document.querySelector(".photo-swiper");
+const beer = document.querySelector(".img-bieres");
 const right = document.getElementById("layer+");
 const left = document.getElementById("layer-");
-
+const rightBeer = document.getElementById("layer++");
+const leftBeer = document.getElementById("layer--");
 function changeNext() {
   right.addEventListener("click", () => {
     slide.scrollLeft += 360;
   });
 }
-
 function changePrevious() {
   left.addEventListener("click", () => {
     slide.scrollLeft -= 360;
   });
 }
+function changeNextBeer() {
+  rightBeer.addEventListener("click", () => {
+    beer.scrollLeft -= 360;
+  });
+}
+function changePreviousBeer() {
+  leftBeer.addEventListener("click", () => {
+    beer.scrollLeft += 360;
+  });
+}
 changeNext();
-changePrevious()
+changePrevious();
+changeNextBeer();
+changePreviousBeer();
