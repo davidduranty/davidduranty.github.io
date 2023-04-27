9a7bf39ee913079845d55b46a84f6a42b30c9c30
DavidDuranty <dadaduranty@gmail.com>
2023-04-27 12:00:43 +0200

    Exercice Site resto intégration grid burgers js

--- Site resto/app.js
+++
@@ -1,8 +0,0 @@
-const btn = document.querySelector("button");
-const gh = document.querySelector("section")
-console.log(carte);
-
-btn.addEventListener("click", () => {
-    console.log("test");
-    gh.classList.toggle("spa")
-})
