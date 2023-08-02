19da5cda8953d1a941f52cfcee9f20f0206f9e2a
DavidDuranty <dadaduranty@gmail.com>
2023-08-02 12:10:00 +0200

    Exercice site vitrine JS avec fetch

---
+++ DomanTea/script.js
@@ -0,0 +1,29 @@
+let userThes = [];
+
+const fetchThe = async () => {
+  await fetch("db.json")
+    .then((res) => res.json())
+    .then((data) => (userThes = data.thes));
+
+  console.log(userThes);
+
+  document.body.innerHTML = userThes
+    .sort((a, b) => a.prix - b.prix)
+    .map(
+      (the) =>
+        `
+      <div class="card">
+        <img src=${the.img} alt="Photo de ${the.name}">
+        <h1>${the.name}</h1><span>⭐⭐⭐</span>
+        <p>Composition : ${the.content}</p>
+        <div class="select">
+          <h4>${the.prix} £ Les 100g</h4>
+          <button class="btn">Ajouter</button>
+        </div>
+      </div>
+    `
+    )
+    .join("");
+};
+
+fetchThe();
