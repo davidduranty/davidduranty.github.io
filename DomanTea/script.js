072dc28f73771fa4662f723a797e839fea3523a3
DavidDuranty <dadaduranty@gmail.com>
2023-08-06 16:11:08 +0200

    Exercice site vitrine THé JS avec fetch suite

--- DomanTea/script.js
+++ DomanTea/script.js
@@ -2,42 +2,46 @@ const container = document.querySelector(".card-container");
 const form = document.querySelector(".form");
 const loupe = document.querySelector(".loupe");
 const input = document.querySelector("input");
+const btns = document.querySelectorAll(".btnSort");
+
 let userThes = [];
-console.log(input);
+let triSort = "minToMax";
 
 const fetchThe = async () => {
   await fetch(`db.json`)
     .then((res) => res.json())
     .then((data) => (userThes = data.thes));
-  
+
   theDisplay();
   console.log(userThes);
-  
-}
+};
 const theDisplay = () => {
   container.innerHTML = userThes
-    .filter((the) => 
+    .filter((the) =>
       the.name.toLowerCase().includes(inputSearch.value.toLowerCase())
     )
-    .sort((a, b) => a.prix - b.prix)
-    .map(
-      (the) =>
-        `
+    .sort((a, b) => {
+      if (triSort === "minToMax") {
+        return a.prix - b.prix;
+      } else if (triSort === "maxToMin") {
+        return b.prix - a.prix;
+      }
+    })
+    .map((the) => {
+      return `
       <div class="card">
-        <img src=${the.img} alt="Photo de ${the.name}">
-        <h1>${the.name}</h1>
-        <p class="compo">Composition : ${the.content}</p>
-        <div class="select">
-          <h4>${the.prix.toFixed(2)}€ Les 100g</h4>
-          <button class="btn">Ajouter</button>
-        </div>
+      <img src=${the.img} alt="Photo de ${the.name}">
+      <h1>${the.name}</h1>
+      <p class="compo">Composition : ${the.content}</p>
+      <div class="select">
+      <h4>${the.prix.toFixed(2)} € <span id="span">/ Les 100g</span></h4>
+      <button class='btn'>${the.btn}</button>
       </div>
-    `
-    )
+      </div>
+      `;
+    })
     .join("");
 };
-
-
 
 loupe.addEventListener("click", (e) => {
   e.preventDefault();
@@ -47,6 +51,13 @@ loupe.addEventListener("click", (e) => {
 const theSearch = () => {
   inputSearch.addEventListener("input", theDisplay);
 };
+
+btns.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    triSort = e.target.id;
+    theDisplay();
+  });
+});
 
 fetchThe();
 theSearch();
