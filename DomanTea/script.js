cee53a01d9e6cbdcad365c548db33eabd9c83232
DavidDuranty <dadaduranty@gmail.com>
2023-08-08 10:10:30 +0200

    Exercice site vitrine THé JS avec fetch suite

--- DomanTea/script.js
+++ DomanTea/script.js
@@ -3,9 +3,23 @@ const form = document.querySelector(".form");
 const loupe = document.querySelector(".loupe");
 const input = document.querySelector("input");
 const btns = document.querySelectorAll(".btnSort");
+const language = document.querySelector(".language");
+const lang = document.querySelectorAll(".lang");
+const nav = document.querySelector(".navbar");
+const body = document.querySelector("body");
+const selectOne = document.querySelector(".select1");
+const selectTwo = document.querySelector(".select2");
+const selectThree = document.querySelector(".select3");
+const selectFour = document.querySelector(".select4");
+const selectFive = document.querySelector(".select5");
+const selectSix = document.querySelector(".select6");
+const croissant = document.querySelector(".croissant");
+const decroissant = document.querySelector(".decroissant");
 
+// const btnAll = document.querySelectorAll(".btn")
+console.log(body);
 let userThes = [];
-let triSort = "minToMax";
+let triSort = "";
 
 const fetchThe = async () => {
   await fetch(`db.json`)
@@ -35,7 +49,7 @@ const theDisplay = () => {
       <p class="compo">Composition : ${the.content}</p>
       <div class="select">
       <h4>${the.prix.toFixed(2)} € <span id="span">/ Les 100g</span></h4>
-      <button class='btn'>${the.btn}</button>
+      <button class='btn'>${(the.btn.onclick = "mafunction();")}</button>
       </div>
       </div>
       `;
@@ -59,5 +73,51 @@ btns.forEach((btn) => {
   });
 });
 
+lang.forEach((el) => {
+  el.addEventListener("click", () => {
+    language.querySelector(".active").classList.remove("active");
+    el.classList.add("active");
+
+    const attr = el.getAttribute("language");
+    selectOne.textContent = data[attr].selection;
+    selectTwo.textContent = data[attr].selection2;
+    selectThree.textContent = data[attr].selection3;
+    selectFour.textContent = data[attr].selection4;
+    selectFive.textContent = data[attr].selection5;
+    selectSix.textContent = data[attr].selection6;
+    btns.textContent = data[attr].bouton;
+    croissant.textContent = data[attr].bouton;
+    decroissant.textContent = data[attr].bouton;
+  });
+});
+
+// btnAll.forEach((bouton) => {
+//   bouton.addEventListener("click", (e) => {
+//     console.log(e.target.id);
+//   })
+// })
 fetchThe();
 theSearch();
+
+const data = {
+  french: {
+    selection: "THÉS & INFUSIONS",
+    selection2: "BOITES & SACHETS",
+    selection3: "COFFRETS & CADEAUX",
+    selection4: "ACCESSOIRES",
+    selection5: "GOURMANDISES",
+    selection6: "FAMILLE DOMAN",
+    input: "Recherche THÉ",
+    bouton: "Prix ↑",
+  },
+  english: {
+    selection: "TEAS & INFUSION",
+    selection2: "BOXES & BAGS",
+    selection3: "GIFT & BOXES",
+    selection4: "ACCESSORIES",
+    selection5: "DELICACIES",
+    selection6: "DOMAN FAMILY",
+    input: "Search Tea",
+    bouton: "Price ↓",
+  },
+};
