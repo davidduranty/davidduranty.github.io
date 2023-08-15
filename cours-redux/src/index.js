8f4b2470ac8de86a1dad814a1d23e00b2b07a262
DavidDuranty <dadaduranty@gmail.com>
2023-08-15 20:31:50 +0200

    Cours Reducer

--- cours-redux/src/index.js
+++ cours-redux/src/index.js
@@ -6,16 +6,21 @@ import "./styles/index.scss";
 // REDUX
 import { Provider } from "react-redux";
 import { configureStore } from "@reduxjs/toolkit";
-import rootReducer from "./reducers"
+import rootReducer from "./reducers";
+import { getPosts } from "./actions/post.action";
+import { getUser } from "./actions/user.action";
 
 const store = configureStore({
-    reducer: rootReducer,
-    devTools: true,
-})
+  reducer: rootReducer,
+  devTools: true,
+});
+
+store.dispatch(getPosts());
+store.dispatch(getUser());
 
 ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
- document.getElementById("root")
+  document.getElementById("root")
 );
--- cours-redux/src/reducers/post.reducer.js
+++ cours-redux/src/reducers/post.reducer.js
@@ -1,6 +1,14 @@
+import { ADD_POST, GET_POSTS } from "../actions/post.action";
 
 const initialState = {};
 
 export default function postReducer(state = initialState, action) {
-    return state;
+    switch (action.type) {
+        case GET_POSTS:
+            return action.payload;
+        case ADD_POST:
+            return [action.payload, ...state];
+        default:
+            return state;
+    }
 }
