dea35ca7dfdc9c59de88383673077644b7fd0d53
DavidDuranty <dadaduranty@gmail.com>
2023-09-01 12:44:51 +0200

    Doman Tea React

--- domantea-react/src/components/Navigation.js
+++ domantea-react/src/components/Navigation.js
@@ -9,54 +9,59 @@ import CoffretKdo from "./CoffretKdo";
 import Accessoires from "./Accessoires";
 import DomanFrere from "./DomanFrere";
 
-
 const Navigation = () => {
-  const [overTeaIfusion, setOverTeaIfusion] = useState(false)
-  const [overBoitesSachets, setOverBoitesSachets] = useState(false)
-  const [overCoffretKdo, setOverCoffretKdo] = useState(false)
-  const [overAccessoires, setOverAccessoires] = useState(false)
-  const [overDoman, setOverDoman] = useState(false)
+  const [overTeaIfusion, setOverTeaIfusion] = useState(false);
+  const [overBoitesSachets, setOverBoitesSachets] = useState(false);
+  const [overCoffretKdo, setOverCoffretKdo] = useState(false);
+  const [overAccessoires, setOverAccessoires] = useState(false);
+  const [overDoman, setOverDoman] = useState(false);
 
   return (
     <div className="navigation-container">
-      <Logo/>
-      <ul className='ul'>
-        <NavLink to="/THES & INFUSIONS">
-          <li className="li" onClick={() => setOverTeaIfusion((prev) => !prev)}>THÉS & INFUSIONS</li>           
+      <Logo />
+      <ul className="ul">
+        <NavLink>
+          <li className="li" onClick={() => setOverTeaIfusion((prev) => !prev)}>
+            THÉS & INFUSIONS
+          </li>
         </NavLink>
-        <NavLink to="/BOITES & SACHETS">
-          <li className="li" onClick={() =>  setOverBoitesSachets((prev) => !prev)}>BOITES & SACHETS</li>
+        <NavLink>
+          <li
+            className="li"
+            onClick={() => setOverBoitesSachets((prev) => !prev)}
+          >
+            BOITES & SACHETS
+          </li>
         </NavLink>
-        <NavLink to="/COFFRETS & CADEAUX">
-          <li className="li" onClick={() => setOverCoffretKdo((prev) => !prev)}>COFFRETS & CADEAUX</li>
+        <NavLink>
+          <li className="li" onClick={() => setOverCoffretKdo((prev) => !prev)}>
+            COFFRETS & CADEAUX
+          </li>
         </NavLink>
-        <NavLink to="/ACCESSOIRES">
-          <li className="li" onClick={() => setOverAccessoires((prev) => !prev)}>ACCESSOIRES</li>
+        <NavLink>
+          <li
+            className="li"
+            onClick={() => setOverAccessoires((prev) => !prev)}
+          >
+            ACCESSOIRES
+          </li>
         </NavLink>
-        <NavLink to="/GOURMANDISES">
+        <NavLink>
           <li className="li">GOURMANDISES</li>
         </NavLink>
-        <NavLink to="/FAMILLE DOMAN">
-          <li className="li" onClick={() => setOverDoman((prev) => !prev)}>FAMILLE DOMAN</li>
+        <NavLink>
+          <li className="li" onClick={() => setOverDoman((prev) => !prev)}>
+            FAMILLE DOMAN
+          </li>
         </NavLink>
       </ul>
-      <Language/>
+      <Language />
       <Icons />
-      {
-        overTeaIfusion &&<TeaInfusion/>
-      }
-      {
-        overBoitesSachets && <BoiteSachets/>
-      }
-      {
-        overCoffretKdo && <CoffretKdo/>
-      }
-      {
-        overAccessoires && <Accessoires/>
-      }
-      {
-        overDoman && <DomanFrere/>
-      }
+      {overTeaIfusion && <TeaInfusion />}
+      {overBoitesSachets && <BoiteSachets />}
+      {overCoffretKdo && <CoffretKdo />}
+      {overAccessoires && <Accessoires />}
+      {overDoman && <DomanFrere />}
     </div>
   );
 };
