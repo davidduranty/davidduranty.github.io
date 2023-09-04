f6b861f296fdddfa78933f61e6820efab200d2f4
DavidDuranty <dadaduranty@gmail.com>
2023-09-04 15:24:58 +0200

    Doman Tea React

---
+++ domantea-react/src/components/SelectionTea.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SelectionTea = () => {
+    return (
+        <div>
+            <h3>Ventes du mois</h3>
+        </div>
+    );
+};
+
+export default SelectionTea;
--- domantea-react/src/components/Swiper.js
+++ domantea-react/src/components/Swiper.js
@@ -1,10 +1,26 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import SelectionTea from "./SelectionTea";
 
 const Swiper = () => {
+  const [thes, setthes] = useState([])
+
+
+  const getData = () => {
+    axios.get("http://localhost:3004/thes").then((res) => setthes(res.data));
+  };
+
+  useEffect(() => getData(), []);
+
   return (
     <div>
       <div className="stars-title">
         <h1>MEILLEURES VENTES DU MOIS</h1>
+        <ul>
+          {thes.map((the) => (
+            <SelectionTea/>
+          ))}
+        </ul>
       </div>
       <div className="swiper-stars-of-month"></div>
       <div className="selection-title">
